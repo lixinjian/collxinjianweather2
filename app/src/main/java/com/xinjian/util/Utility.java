112a41f4a24f5ec53199e976a5d007c5f9e83029
lixinjian <lixinjian00>
2017-06-06 22:15:17 +0800

    测试

--- app/src/main/java/com/xinjian/util/Utility.java
+++ app/src/main/java/com/xinjian/util/Utility.java
@@ -93,7 +93,7 @@ public class Utility {
     public static Weather handleWeatherResponse(String response){
         try {
             JSONObject jsonObject = new JSONObject(response);
-            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather5");
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
             String weatherContent = jsonArray.getJSONObject(0).toString();
             return new Gson().fromJson(weatherContent,Weather.class);
         }catch (Exception e){
